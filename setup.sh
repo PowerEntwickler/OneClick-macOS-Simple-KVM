31b9aa2622f69244e4706bc66e5cda12495ac342
swaggyP36000 <45519513+swaggyP36000@users.noreply.github.com>
2022-02-02 10:01:51 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ qemu-img create -f qcow2 macOS.qcow2 64G
 
 ./fetch-macOS-v2.py
 
+qemu-img convert BaseSystem.dmg -O raw BaseSystem.img
+
 (ls RecoveryImage.dmg >> /dev/null 2>&1 && mv RecoveryImage.dmg BaseSystem.dmg) || echo ""
 
 sudo ./basic.sh
