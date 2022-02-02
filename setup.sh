819d0e189f753a44e31044df2ce4d377ae6b64fa
swaggyP36000 <45519513+swaggyP36000@users.noreply.github.com>
2022-02-02 10:01:07 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get install qemu-system qemu-utils python python3-pip -y  # for Ubuntu,
 
 qemu-img create -f qcow2 macOS.qcow2 64G
 
-./fetch-macOS.py
+./fetch-macOS-v2.py
 
 (ls RecoveryImage.dmg >> /dev/null 2>&1 && mv RecoveryImage.dmg BaseSystem.dmg) || echo ""
 
